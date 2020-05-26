a457adbe6d4cbbc621ad5742272859fa21d868d5
Fernando Libório <fernando.liborio@telefonica.com>
2020-05-26 16:14:47 -0300

    new concept js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/* SPA Strategy*/
 (function() {
     
     var partialsCache = {};
@@ -57,3 +57,124 @@
     window.addEventListener("hashchange", navigate);
 
 }());
+
+/* Send Email via SMTPJS */
+function sendMail() {
+    document.getElementById('contact-form').addEventListener('submit', async function (event) {
+        event.preventDefault();
+        const output = `
+        <p>You have a new contact request</p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${this.name.value}</li>
+            <li>Fone: ${this.fone.value}</li>
+            <li>Email: ${this.email.value}</li>
+        </ul>
+        <h3>Message</h3>
+        <p>${this.textMessage.value}</p>
+        `;
+        const nome = this.name.value;
+        loading = document.querySelector('.loading');
+        loading.style.display = 'block';
+        await Email.send({
+            SecureToken: "6b70121e-f387-4e72-8801-8a63a3224cbd",
+            To: 'nandotromp@gmail.com',
+            From: "emporiosaudecuritiba@gmail.com",
+            Subject: this.subject.value,
+            Body: output
+        }).then(
+            /* message => alert(`Obrigado pela visita Sr(a) ${nome}, entrarei em contato o mais breve possível!`) */
+            message => modalSet('modal', `Sr(a) ${nome}, entrarei em contato o mais breve possível! Tenha um excelente dia.`),
+            document.getElementById('contact-form').reset()
+        );
+    });
+}
+
+/* SmtpJS.com - v3.0.0 */
+var Email = {
+    send: function (a) {
+        return new Promise(function (n, e) {
+            a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send";
+            var t = JSON.stringify(a);
+            Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, 
+            function (e) { n(e) })
+        })
+    }, ajaxPost: function (e, n, t) {
+        var a = Email.createCORSRequest("POST", e);
+        a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), 
+        a.onload = function () {
+            var e = a.responseText; null != t && t(e)
+        }, a.send(n)
+    }, ajax: function (e, n) {
+        var t = Email.createCORSRequest("GET", e);
+        t.onload = function () {
+            var e = t.responseText; null != n && n(e)
+        }, t.send()
+    }, createCORSRequest: function (e, n) {
+        var t = new XMLHttpRequest;
+        return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t
+    }
+};
+
+/* Modal Confirm */
+function modalSet(id, msg) {
+    const submitBtn = document.getElementById('btnSendMail');
+    document.getElementById("return").innerHTML = msg;
+    $("#".concat(id)).modal("show");
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == id || e.target.className == 'btn btn-danger' || e.target.className == 'close') {
+            $("#".concat(id)).modal("hide");
+            loading.style.display = 'none';
+            submitBtn.setAttribute('disabled', true);
+        }
+    });
+}
+
+/* Fields Validation */
+function validateFields() {
+    document.getElementById('name').addEventListener('input', function (e) {
+        let count = '';
+        span_name = document.querySelector('.msg-name');
+        e.target.value = e.target.value.replace(/[^ a-zA-Z]+/g, '');
+        if (e.target.value.length < 3) {
+            span_name.innerHTML = "Preenchimento mínimo de 3 caracteres!";
+            span_name.style.display = 'block';
+            count += 1;
+        } else {
+            span_name.style.display = 'none';
+            validator(count);
+        }
+    });
+    document.getElementById('email').addEventListener('input', function (e) {
+        let count = '';
+        span_email = document.querySelector('.msg-email');
+        const testEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        if (e.target.value.length > 3) {
+            if (testEmail.test(e.target.value)) {
+                span_email.style.display = 'none';
+            } else {
+                span_email.innerHTML = "Formato do E-mail inválido";
+                span_email.style.display = 'block';
+                count += 1;
+            }
+            validator(count);
+        }
+    });
+}
+
+function validator(count) {
+    const invalidForm = document.querySelector('form:invalid');
+    const submitBtn = document.getElementById('btnSendMail');
+    if (invalidForm || count != '') {
+        submitBtn.setAttribute('disabled', true);
+    } else {
+        submitBtn.disabled = false;
+    }
+}
+
+function mask() {
+    document.getElementById('fone').addEventListener('input', function (e) {
+        var aux = e.target.value.length < 15 ? e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,4})(\d{0,4})/) : e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+        e.target.value = !aux[2] ? aux[1] : '(' + aux[1] + ') ' + aux[2] + (aux[3] ? '-' + aux[3] : '');
+    });
+}
